8569a47adecb8f7267d26c8a3cb3b8a494c25eff
Muhammad Azeem <120501480+MUHAMMAD-AZEEM-AZAM@users.noreply.github.com>
2025-06-26 14:31:47 +0500

    Update Layout.tsx
    remove class "bg-white border-b"

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,7 +12,7 @@ const Layout = ({ children }: LayoutProps) => {
     <div className="flex h-screen bg-gray-50">
       <Sidebar />
       <div className="flex-1 flex flex-col">
-        <header className="bg-white border-b px-6 py-4">
+        <header className="px-6 py-4">
           <div className="flex items-center justify-between">
             <div className="relative flex-1 max-w-2xl">
               <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
